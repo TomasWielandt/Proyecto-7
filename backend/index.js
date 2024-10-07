8372221c6b9c6b26ffa6064d741f57092ee2ba82
TomasWielandt <twielandt16@gmail.com>
2024-10-07 17:37:01 -0300

    se agrega ruteo en index.js

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 
+// ruteo
+const routes = require('./routes');
+app.use(process.env.URL_BASE + "/", routes);
+
 // conexión a la base de datos
 connectDB();
 
