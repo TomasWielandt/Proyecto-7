2e35fae0b09bbf6bfaaa151121792447c42c4bfb
TomasWielandt <twielandt16@gmail.com>
2024-10-07 13:05:40 -0300

    se crea la carpeta config con el archivo db.js y se configura en index.js la conección a la base de datos

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const cors = require('cors');
 
+const connectDB = require('./config/db');
+
 const app = express();
 
 // middlewares
@@ -10,6 +12,9 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
+
+// conexión a la base de datos
+connectDB();
 
 // servidor
 app.listen(process.env.PORT || 3000, () => {
