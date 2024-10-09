6a0ef64f17ca8a5b7893265a2a87290ea9b637b3
TomasWielandt <twielandt16@gmail.com>
2024-10-09 14:33:45 -0300

    se actualiza PayPalButton.jsx

--- frontend/src/components/PayPalButton.jsx
+++ frontend/src/components/PayPalButton.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { PayPalButtons } from '@paypal/react-paypal-js';
 
-const PayPalButton = () => {
+const PayPalButton = ({ total }) => {
   return (
     <PayPalButtons
       createOrder={(data, actions) => {
         return actions.order.create({
           purchase_units: [{
             amount: {
-              value: '0.01', // Cambia esto por el valor real
+              value: total, // Utilizamos el total que se recibe por props
             },
           }],
         });
@@ -16,7 +16,7 @@ const PayPalButton = () => {
       onApprove={async (data, actions) => {
         const order = await actions.order.capture();
         console.log('Order', order);
-        // Aquí puedes manejar la respuesta de PayPal
+        // Aquí puedes manejar la respuesta de PayPal y completar el flujo de la compra
       }}
     />
   );
