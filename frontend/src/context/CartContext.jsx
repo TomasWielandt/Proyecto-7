7beaeafb7b126e9d39a757a9cc31c9ca44a83843
TomasWielandt <twielandt16@gmail.com>
2024-10-09 14:37:07 -0300

    se actualiza CartContext.jsx

--- frontend/src/context/CartContext.jsx
+++ frontend/src/context/CartContext.jsx
@@ -4,15 +4,32 @@ const CartContext = createContext();
 
 const initialState = {
   items: [],
-  total: 0,
 };
 
 const cartReducer = (state, action) => {
   switch (action.type) {
     case 'ADD_ITEM':
-      // Lógica para agregar un artículo
-      return { ...state, items: [...state.items, action.payload] };
-    // Agregar otros casos según sea necesario
+      const itemExists = state.items.find(item => item._id === action.payload._id);
+      if (itemExists) {
+        return {
+          ...state,
+          items: state.items.map(item => 
+            item._id === action.payload._id 
+              ? { ...item, quantity: item.quantity + 1 }
+              : item
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          items: [...state.items, { ...action.payload, quantity: 1 }],
+        };
+      }
+    case 'REMOVE_ITEM':
+      return {
+        ...state,
+        items: state.items.filter(item => item._id !== action.payload),
+      };
     default:
       return state;
   }
@@ -20,7 +37,7 @@ const cartReducer = (state, action) => {
 
 export const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(cartReducer, initialState);
-  
+
   return (
     <CartContext.Provider value={{ state, dispatch }}>
       {children}
