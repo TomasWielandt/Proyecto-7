fd8744b4f5aa89856525e900cafe3d692ff2a00c
TomasWielandt <twielandt16@gmail.com>
2024-10-09 12:57:20 -0300

    se crea el archivo Navbar.jsx dentro de components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import SignUp from './pages/SignUp';
-import LogIn from './pages/LogIn';
-import Profile from './pages/Profile';
-import ProductList from './pages/ProductList';
-import ProductDetail from './pages/ProductDetail';
-import Cart from './pages/Cart';
+// import Home from './pages/Home';
+// import SignUp from './pages/SignUp';
+// import LogIn from './pages/LogIn';
+// import Profile from './pages/Profile';
+// import ProductList from './pages/ProductList';
+// import ProductDetail from './pages/ProductDetail';
+// import Cart from './pages/Cart';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import ErrorBoundary from './components/ErrorBoundary';
+// import Footer from './components/Footer';
+// import ErrorBoundary from './components/ErrorBoundary';
 
 const App = () => {
   return (
     <ErrorBoundary>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<LogIn />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/products" element={<ProductList />} />
         <Route path="/product/:id" element={<ProductDetail />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route path="/cart" element={<Cart />} /> */}
       </Routes>
       <Footer />
     </ErrorBoundary>
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 p-4">
+      <ul className="flex space-x-4">
+        <li><Link to="/" className="text-white">Home</Link></li>
+        <li><Link to="/signup" className="text-white">Sign Up</Link></li>
+        <li><Link to="/login" className="text-white">Log In</Link></li>
+        <li><Link to="/products" className="text-white">Products</Link></li>
+        <li><Link to="/cart" className="text-white">Cart</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
