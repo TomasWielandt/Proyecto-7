03a70f10a65789d96718ab8a0239bb448b348646
TomasWielandt <twielandt16@gmail.com>
2024-10-10 16:03:30 -0300

    se actualiza ProductCard.jsx

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -7,7 +7,7 @@ const ProductCard = ({ product }) => {
     <div className="border p-4">
       <img src={product.image} alt={product.name} className="w-full h-32 object-cover" />
       <h3 className="text-lg">{product.name}</h3>
-      <p>{product.description}</p>
+      <p>${product.price}</p>
       <Link to={`/products/${product._id}`} className="text-blue-500">Ver detalles</Link>
     </div>
   );
@@ -17,7 +17,7 @@ ProductCard.propTypes = {
   product: PropTypes.shape({
     _id: PropTypes.string.isRequired,         // El ID del producto es requerido y debe ser una cadena
     name: PropTypes.string.isRequired,        // El nombre del producto es requerido
-    description: PropTypes.string.isRequired, // La descripción del producto es requerida
+    price: PropTypes.number.isRequired, // La descripción del producto es requerida
     image: PropTypes.string,  // Ahora 'image' es opcional
   }).isRequired
 };
