70ab8083aa666fa87f5c96f82eb850afa5f3a8a7
TomasWielandt <twielandt16@gmail.com>
2024-10-11 13:03:05 -0300

    se ajusta el tamaño de la imagen y el contenedor en ProductCard

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -4,12 +4,16 @@ import PropTypes from 'prop-types';
 
 const ProductCard = ({ product }) => {
   return (
-    <div className="border p-4">
+    <div className="border p-4 rounded-lg shadow-lg">
       {/* Mostrar la imagen utilizando imageUrl */}
       {product.imageUrl && (
-        <img src={product.imageUrl} alt={product.name} className="w-full h-32 object-cover" />
+        <img 
+          src={product.imageUrl} 
+          alt={product.name} 
+          className="w-full h-80 object-contain" // Aumentamos la altura a 'h-80' para mejorar proporciones
+        />
       )}
-      <h3 className="text-lg">{product.name}</h3>
+      <h3 className="text-lg mt-4">{product.name}</h3>
       <p>${product.price}</p>
       <Link to={`/products/${product._id}`} className="text-blue-500">Ver detalles</Link>
     </div>
