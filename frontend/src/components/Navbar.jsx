907fd0f6e1674b94af3b6e7d1be7ac131eac0136
TomasWielandt <twielandt16@gmail.com>
2024-10-11 16:39:44 -0300

    se centra el icono de carrito en Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -35,13 +35,21 @@ const Navbar = () => {
           </button>
         </div>
 
-        {/* Botón para el menú en pantallas pequeñas (solo visible en pantallas pequeñas) */}
-        <button
-          className="text-white md:hidden col-span-2" // Solo visible en pantallas pequeñas
-          onClick={toggleMenu}
-        >
-          {isOpen ? 'Cerrar' : 'Menu'}
-        </button>
+        {/* Botón para el menú en pantallas pequeñas (centrado y solo visible en pantallas pequeñas) */}
+        <div className="col-span-2 md:hidden flex justify-center"> 
+          <button
+            className="text-white" // Solo visible en pantallas pequeñas
+            onClick={toggleMenu}
+          >
+            {isOpen ? 'Cerrar' : (
+              <img 
+                src="https://res.cloudinary.com/dfxlipbvl/image/upload/v1728674751/list_17025562_htxjxv.png" 
+                alt="Menú" 
+                className="h-8 w-auto"
+              />
+            )}
+          </button>
+        </div>
 
         {/* Sección de enlaces de navegación (col-span 3 y 4) */}
         <ul className={`col-span-2 md:flex md:justify-end md:space-x-6 items-center ${isOpen ? 'flex flex-col' : 'hidden md:flex'} bg-gray-800 md:bg-transparent`}>
