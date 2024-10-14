685bbcdf4812a5491553666ca0604a006069c51b
TomasWielandt <twielandt16@gmail.com>
2024-10-14 01:32:07 -0300

    se vuelve a dejar configuracion inicial de target en vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,8 @@ export default {
   server: {
     proxy: {
       '/api': {
-        // target: 'http://localhost:3000', // Cambia el puerto si es necesario
-        target: 'https://proyecto-7.onrender.com',
+        target: 'http://localhost:3000', // Cambia el puerto si es necesario
+        // target: 'https://proyecto-7.onrender.com',
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ''),
       },
